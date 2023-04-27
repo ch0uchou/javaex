75ca1b9edae3ac5f4f6911b8871011843f169a0b
chouchou <chaudiemhoang2@gmail.com>
2023-04-27 21:25:18 +0700

    #

--- Test1.java
+++ Test1.java
@@ -1,28 +1,70 @@
-import java.awt.Dimension;
-
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+ 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-
+import javax.swing.WindowConstants;
+ 
 public class Test1 {
-
-    public static void main(String... args) {
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        for (int i = 0; i < 10; i++) {
-            panel.add(new JButton("Hello-" + i));
+    private JFrame mainFrame;
+    private JLabel headerLabel;
+    private JLabel statusLabel;
+    private JPanel controlPanel;
+ 
+    public Test1(){
+       prepareGUI();
+    }
+ 
+    public static void main(String[] args) {
+        Test1 demo = new Test1();
+        demo.showFocusListenerDemo();
+    }
+ 
+    private void prepareGUI() {
+        mainFrame = new JFrame("Vi du Java Swing");
+        mainFrame.setSize(500, 300);
+        mainFrame.setLayout(new GridLayout(3, 1));
+        headerLabel = new JLabel("", JLabel.CENTER);
+        statusLabel = new JLabel("", JLabel.CENTER);
+        statusLabel.setSize(350, 100);
+        controlPanel = new JPanel();
+        controlPanel.setLayout(new FlowLayout());
+        mainFrame.add(headerLabel);
+        mainFrame.add(controlPanel);
+        mainFrame.add(statusLabel);
+        mainFrame.setVisible(true);
+        mainFrame.setTitle("Ví dụ FocusListener trong Java Swing");
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+ 
+    private void showFocusListenerDemo() {
+        headerLabel.setText("Listener in action: FocusListener");
+        JButton okButton = new JButton("OK");
+        JButton cancelButton = new JButton("Cancel");
+ 
+        okButton.addFocusListener(new CustomFocusListener());
+        cancelButton.addFocusListener(new CustomFocusListener());
+ 
+        controlPanel.add(okButton);
+        controlPanel.add(cancelButton);
+        mainFrame.setVisible(true);
+    }
+ 
+    class CustomFocusListener implements FocusListener {
+        public void focusGained(FocusEvent e) {
+            statusLabel.setText(statusLabel.getText() 
+                    + e.getComponent().getClass().getSimpleName() 
+                    + " gained focus. ");
         }
-        JScrollPane scrollPane = new JScrollPane(panel);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollPane.setBounds(10, 30, 1000, 100);
-        JPanel contentPane = new JPanel(null);
-        contentPane.setPreferredSize(new Dimension(200, 400));
-        contentPane.add(scrollPane);
-        frame.add(contentPane);
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setVisible(true);
+ 
+        public void focusLost(FocusEvent e) {
+            statusLabel.setText(statusLabel.getText() 
+                    + e.getComponent().getClass().getSimpleName() 
+                    + " lost focus. ");
+        }
     }
 }
